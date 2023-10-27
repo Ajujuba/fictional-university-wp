8ec30e29ee4f9e3264bdb7f0cce8d71f85b75636
Ana Alves <60891489+Ajujuba@users.noreply.github.com>
2023-10-27 10:47:03 +0100

    feat: add a custom block with university numbers
    - Create a new block on my homepage with my block layout
    - Add my style with scss
    - Create a new page to control my numbers with custom fields (ACF)
    - Call custom fields in my homepage to always be  up to date
    - Adjust to other styles and fix my layout on mobile

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -154,6 +154,49 @@ $page2 = get_page_by_path('content-2');
     </div>
 </div>
 
+
+<div class="row-perso">
+    <div class="left-column">
+        <div class="left-top">
+            <div class="square">
+                <div class="number"><?= get_field('our_students', 223) ?></div>
+                <div class="subtitle-number">
+                    Our <strong>students</strong>
+                </div>
+            </div>
+            <div class="square">
+                <div class="number"><?= get_field('our_campuses', 223) ?></div>
+                <div class="subtitle-number">
+                    <strong>campuses</strong><br/>
+                    around the world
+                </div>
+            </div>
+        </div>
+        <div class="left-bottom">
+            <div class="square">
+                <div class="number"><?= get_field('our_courses', 223) ?></div>
+                <div class="subtitle-number">
+                    Different <strong>courses</strong><br/>
+                    for you
+                </div>
+            </div>
+            <div class="square">
+                <div class="number"><?= get_field('our_professors', 223) ?></div>
+                <div class="subtitle-number">
+                    Trained <strong>professors</strong>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="right-column">
+        <div class="box-text">
+            <div class="col-text">Our Numbers</div>
+        </div>
+    </div>
+</div>
+
+
 <?php
 get_footer();
 ?>
