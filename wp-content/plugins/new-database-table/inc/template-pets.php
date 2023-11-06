ef99070ce423c8f83249f9df87fd27edd06fe27b
Ana Alves <60891489+Ajujuba@users.noreply.github.com>
2023-11-06 11:37:39 +0000

    feat: Add a new plugin to create custom SQL DB table v1
    - Add a new plugin to create a new custom table in my DB
    - Add a file to create random animals to save in my new custom table
    - Add a custom query to select and show in my table my pets from my database

---
+++ wp-content/plugins/new-database-table/inc/template-pets.php
@@ -0,0 +1,52 @@
+<?php
+
+get_header(); ?>
+
+<div class="page-banner">
+  <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+  <div class="page-banner__content container container--narrow">
+    <h1 class="page-banner__title">Pet Adoption</h1>
+    <div class="page-banner__intro">
+      <p>Providing forever homes one search at a time.</p>
+    </div>
+  </div>  
+</div>
+
+<div class="container container--narrow page-section">
+
+  <p>This page took <strong><?php echo timer_stop();?></strong> seconds to prepare. Found <strong>x</strong> results (showing the first x).</p>
+
+  <?php
+    global $wpdb;
+
+    $tablename = $wpdb->prefix . 'pets';
+    $sql = $wpdb->prepare("SELECT * FROM $tablename LIMIT 100");
+    $pets = $wpdb->get_results($sql);
+    //print_r($pets);
+  ?>
+  <table class="pet-adoption-table">
+    <tr>
+      <th>Name</th>
+      <th>Species</th>
+      <th>Weight</th>
+      <th>Birth Year</th>
+      <th>Hobby</th>
+      <th>Favorite Color</th>
+      <th>Favorite Food</th>
+    </tr>
+    <?php foreach($pets as $pet): ?>
+      <tr>
+        <td><?= $pet->petname ?></td>
+        <td><?= $pet->species ?></td>
+        <td><?= $pet->petweight ?></td>
+        <td><?= $pet->birthyear ?></td>
+        <td><?= $pet->favhobby ?></td>
+        <td><?= $pet->favcolor ?></td>
+        <td><?= $pet->favfood ?></td>
+      </tr>
+    <?php endforeach ?>
+  </table>
+  
+</div>
+
+<?php get_footer(); ?>
---
+++ wp-content/plugins/new-database-table/new-database-table.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+  Plugin Name: Pet Adoption (New DB Table)
+  Version: 1.0
+  Author: Brad
+  Author URI: https://www.udemy.com/user/bradschiff/
+*/
+
+if( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+require_once plugin_dir_path(__FILE__) . 'inc/generatePet.php';
+
+class PetAdoptionTablePlugin {
+  function __construct() {
+    global $wpdb; //global variable $wpdb, which is the main instance of the WordPress $wpdb class used to interact with the database
+    $this->charset = $wpdb->get_charset_collate(); //This line is getting the character set and collation from the database
+    $this->tablename = $wpdb->prefix . "pets"; //This is defining the name of the table to be created in the database. It uses the database prefix configured in WordPress
+
+    add_action(
+      'activate_new-database-table/new-database-table.php', //here the hook is: activate_yourPluginFolderName_YourMainFileName.php
+      array($this, 'onActivate') //fn that will be called when activating the plugin
+    ); //This hook is executed 1x only when my plugin is activated 
+    //add_action('admin_head', array($this, 'onAdminRefresh')); //run in Admin when I reaload the page to add my pets, if If call the function onAdminRefresh only 1 pet will be added, but if I run PopulatedFast many pets will be added
+    add_action('wp_enqueue_scripts', array($this, 'loadAssets')); //run in my front
+    add_filter('template_include', array($this, 'loadTemplate'), 99); //This is a hook that allows you to modify the page template used when loading a page in WordPress. It calls the loadTemplate() function and sets the priority to 99
+  }
+
+  function onActivate() {
+    require_once(ABSPATH . 'wp-admin/includes/upgrade.php'); //This includes the WordPress upgrade.php file, which contains the dbDelta() function. This function is used to update the database structure.
+    dbDelta("CREATE TABLE $this->tablename (
+      id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+      birthyear smallint(5) NOT NULL DEFAULT 0,
+      petweight smallint(5) NOT NULL DEFAULT 0,
+      favfood varchar(60) NOT NULL DEFAULT '',
+      favhobby varchar(60) NOT NULL DEFAULT '',
+      favcolor varchar(60) NOT NULL DEFAULT '',
+      petname varchar(60) NOT NULL DEFAULT '',
+      species varchar(60) NOT NULL DEFAULT '',
+      PRIMARY KEY  (id)
+    ) $this->charset;");//this fn dbDelta is very sensitive, so is important follow all rules of WP documentation to create new tables
+  }
+
+  function onAdminRefresh() {
+    global $wpdb;
+    $wpdb->insert(
+      $this->tablename, //name of your table
+      generatePet() //datas that you want to save in the DB
+    );
+  }
+
+  function loadAssets() {
+    if (is_page('pet-adoption')) {
+      wp_enqueue_style('petadoptioncss', plugin_dir_url(__FILE__) . 'pet-adoption.css');
+    }
+  }
+
+  function loadTemplate($template) {
+    if (is_page('pet-adoption')) {
+      return plugin_dir_path(__FILE__) . 'inc/template-pets.php'; //if my user open the page with slug 'pet-adoption' I'll call my custom template, else I'll call the default template
+    }
+    return $template;
+  }
+
+  function populateFast() {
+    $query = "INSERT INTO $this->tablename (`species`, `birthyear`, `petweight`, `favfood`, `favhobby`, `favcolor`, `petname`) VALUES ";
+    $numberofpets = 100;
+    for ($i = 0; $i < $numberofpets; $i++) {
+      $pet = generatePet();
+      $query .= "('{$pet['species']}', {$pet['birthyear']}, {$pet['petweight']}, '{$pet['favfood']}', '{$pet['favhobby']}', '{$pet['favcolor']}', '{$pet['petname']}')";
+      if ($i != $numberofpets - 1) {
+        $query .= ", ";
+      }
+    }
+    /*
+    Never use query directly like this without using $wpdb->prepare in the
+    real world. I'm only using it this way here because the values I'm 
+    inserting are coming fromy my innocent pet generator function so I
+    know they are not malicious, and I simply want this example script
+    to execute as quickly as possible and not use too much memory.
+    */
+    global $wpdb;
+    $wpdb->query($query);
+  }
+
+}
+
+$petAdoptionTablePlugin = new PetAdoptionTablePlugin();
