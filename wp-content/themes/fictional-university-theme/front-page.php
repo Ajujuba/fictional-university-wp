7bae6df86743cdee925b11257919ef3d09a051d1
Ana Alves <60891489+Ajujuba@users.noreply.github.com>
2024-01-29 17:18:36 +0000

    feat: add test with donorbox
    - add a form for donorBox and make js validations

--- wp-content/themes/fictional-university-theme/front-page.php
+++ wp-content/themes/fictional-university-theme/front-page.php
@@ -196,7 +196,90 @@ $page2 = get_page_by_path('content-2');
     </div>
 </div>
 
+<div class="test-donation" style="display: flex;justify-content: space-around;margin: 30px 0px;">
+    <div class="left-colum">
+        <script src="https://donorbox.org/widget.js" paypalExpress="false"></script><iframe src="https://donorbox.org/embed/donor-test-3" name="donorbox" allowpaymentrequest="allowpaymentrequest" seamless="seamless" frameborder="0" scrolling="no" height="900px" width="100%" style="max-width: 500px; min-width: 350px; max-height:none!important"></iframe>
+    </div>
+
+    <style>
+        form#FormDonate {
+            background-color: aliceblue;
+            width: 300px;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+        }
+        form#FormDonate input{
+            padding: 10px;
+            margin: 10px 0px;
+        }
+    </style>
+    <div class="right-colum">
+        <form id="FormDonate">
+            <div class="row-radio">
+                <input type="radio" name="radioAmount" value="5">R$5
+                <input type="radio" name="radioAmount" value="10">R$10
+                <input type="radio" name="radioAmount" value="20">R$20
+            </div>
+            <input type="text" id="amount" name="amount" placeholder="Ex: R$200.000,00" required>
+            <input type="text" id="first_name"  name="first_name" placeholder="Your name" required>
+            <input type="text" id="last_name" name="last_name" placeholder="Your last name" required>
+            <input type="email"  id="email" name="email" placeholder="Your email" required>
+            <br>
+            <button type="button" onclick="validarFormulario()">Enviar para Donorbox</button>
+        </form>
+    </div>
+</div>
 
 <?php
 get_footer();
 ?>
+
+<script>
+    function validarFormulario() {
+
+        var firstName = document.getElementById('first_name').value;
+        var lastName = document.getElementById('last_name').value;
+        var email = document.getElementById('email').value;
+        var amount = document.getElementById('amount').value;
+
+        var radioValue = document.querySelector('input[name="radioAmount"]:checked');
+        // Verificar se o usuário escolheu uma opção de radio OU inseriu um valor no campo de texto
+        if (!radioValue && amount.trim() === '') {
+            alert('Por favor, escolha uma opção de doação ou insira um valor.');
+            return false;
+        }
+        // Se o usuário escolheu uma opção de radio, usar o valor do radio, senão, usar o valor do campo de texto
+        var selectedAmount = radioValue ? radioValue.value : amount;
+        
+        if (firstName.trim() === '' || lastName.trim() === '' || email.trim() === '') {
+            alert('Por favor, preencha todos os campos.');
+            return false;
+        }
+
+        var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if (!emailRegex.test(email)) {
+            alert('Por favor, insira um endereço de e-mail válido.');
+            return false;
+        }
+
+        enviarParaDonorbox(selectedAmount);
+    }
+
+    function enviarParaDonorbox(amount) {
+        // Obter os valores dos campos do formulário
+        var firstName = document.getElementById('first_name').value;
+        var lastName = document.getElementById('last_name').value;
+        var email = document.getElementById('email').value;
+
+        // Construir a URL com os parâmetros preenchidos
+        var donorboxURL = "https://donorbox.org/embed/donor-test-3?";
+        donorboxURL += "first_name=" + encodeURIComponent(firstName);
+        donorboxURL += "&last_name=" + encodeURIComponent(lastName);
+        donorboxURL += "&email=" + encodeURIComponent(email);
+        donorboxURL += "&amount=" + encodeURIComponent(amount);
+
+        // Redirecionar para a URL da Donorbox
+        window.location.href = donorboxURL;
+    }
+</script>
