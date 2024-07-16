84c40ea8ae6138766ac300e60778a8a55b683415
Ana Alves <60891489+Ajujuba@users.noreply.github.com>
2024-07-16 15:29:07 +0100

    feat: add alert when don't find the coordenates
    - In the the first time that try load the address, appears an error if don't find

--- wp-content/themes/fictional-university-theme/assets/js/custom-script-automatic-coord.js
+++ wp-content/themes/fictional-university-theme/assets/js/custom-script-automatic-coord.js
@@ -1,8 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
     var lat =  document.querySelector('[data-name="latitude"] input');
+    var alertShown = false;
 
     if(lat){
         lat.addEventListener('focus', () => {
+            if (alertShown) return; //ensures that the alert only appears once
+
             const endereco = `${document.querySelector('[data-name="address"] input').value}, ${document.querySelector('[data-name="city"] input').value}, ${document.querySelector('[data-name="country"] input').value}, ${document.querySelector('[data-name="postal_code"] input').value}`;
             console.log(endereco)
 
@@ -22,6 +25,8 @@ document.addEventListener("DOMContentLoaded", function() {
                     console.log('Generated coordinates:', latitude, longitude);
                 } else {
                     console.error('No coordinates found for the address provided.');
+                    alert('Sorry, no coordinates found for the address provided. Please check the address or paste the coordinates directly into the fields.');
+                    alertShown = true;
                 }
             })
             .catch(error => {
